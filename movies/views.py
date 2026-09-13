65b9810ae0b6d961760e60c8088b18978b46297d
nthuppanna <niharika.thuppanna@gmail.com>
2026-09-13 09:21:54 -0400

    add reporting reviews

--- movies/views.py
+++ movies/views.py
@@ -18,13 +18,13 @@ def index(request):
 
 def show(request, id):
     movie = Movie.objects.get(id=id)
-    reviews = Review.objects.filter(movie=movie)
+    reviews = Review.objects.filter(movie=movie, reported=False)
     template_data = {}
     template_data['title'] = movie.name
     template_data['movie'] = movie
     template_data['reviews'] = reviews
     return render(request, 'movies/show.html',
-                  {'template_data': template_data})
+        {'template_data': template_data})
 
 @login_required
 def create_review(request, id):
@@ -63,4 +63,11 @@ def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id,
         user=request.user)
     review.delete()
+    return redirect('movies.show', id=id)
+
+@login_required
+def report_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    review.reported = True
+    review.save()
     return redirect('movies.show', id=id)
